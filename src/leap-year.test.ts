9cca8377b41c99b0270528bfd242a02b4ea3f40f
Rakesh Kumar <rakesh@incubyte.co>
2025-02-05 13:11:25 +0530

    refactor: tests for calendar system changes

--- src/leap-year.test.ts
+++ src/leap-year.test.ts
@@ -1,25 +1,27 @@
 import { DivisibilityChecker, GregorianCalender, LeapYearChecker } from "./leap-year";
 
 describe("LeapYearChecker", () => {
-  let leapYearChecker: LeapYearChecker;
-  beforeEach(() => {
-    let gergorianCalendar = new GregorianCalender(new DivisibilityChecker());
-    leapYearChecker = new LeapYearChecker(gergorianCalendar);
-  });
+  const divisibilityChecker = new DivisibilityChecker();
+  describe("with Gregorian Calendar", () => {
+    let leapYearChecker: LeapYearChecker;
+    beforeEach(() => {
+      leapYearChecker = new LeapYearChecker(new GregorianCalender(divisibilityChecker));
+    });
 
-  it("should return false when number is not divisible by 4", () => {
-    expect(leapYearChecker.isLeapYear(1997)).toBe(false);
-  });
+    it("should return false when number is not divisible by 4", () => {
+      expect(leapYearChecker.isLeapYear(1997)).toBe(false);
+    });
 
-  it("should return true when year is divisible by 4", () => {
-    expect(leapYearChecker.isLeapYear(1996)).toBe(true);
-  });
+    it("should return true when year is divisible by 4", () => {
+      expect(leapYearChecker.isLeapYear(1996)).toBe(true);
+    });
 
-  it("should return true when a year is divisible by 400", () => {
-    expect(leapYearChecker.isLeapYear(1600)).toBe(true);
-  });
+    it("should return true when a year is divisible by 400", () => {
+      expect(leapYearChecker.isLeapYear(1600)).toBe(true);
+    });
 
-  it("should return false when a year is divisible by 4 but not 100", () => {
-    expect(leapYearChecker.isLeapYear(1800)).toBe(false);
+    it("should return false when a year is divisible by 4 but not 100", () => {
+      expect(leapYearChecker.isLeapYear(1800)).toBe(false);
+    });
   });
 });
