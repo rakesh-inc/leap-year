f7385bdc362a198963cf8179da432b09e9fdcd50
Rakesh Kumar <rakesh@incubyte.co>
2025-02-05 12:24:53 +0530

    test: year not divisible by 4

---
+++ src/leap-year.test.ts
@@ -0,0 +1,6 @@
+describe("LeapYearChecker", () => {
+  it("should return false when number is not divisible by 4", () => {
+    const leapYearChecker = new LeapYearChecker();
+    expect(leapYearChecker.isLeapYear(1997)).toBe(false);
+  });
+});
