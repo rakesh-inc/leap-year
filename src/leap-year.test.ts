af0de0a41301ab3cee88bf9176e38575782865f4
Rakesh Kumar <rakesh@incubyte.co>
2025-02-05 12:32:57 +0530

    refactor: implement class inialization using beforeEach

--- src/leap-year.test.ts
+++ src/leap-year.test.ts
@@ -1,13 +1,16 @@
 import { LeapYearChecker } from "./leap-year";
 
 describe("LeapYearChecker", () => {
+  let leapYearChecker: LeapYearChecker;
+  beforeEach(() => {
+    leapYearChecker = new LeapYearChecker();
+  });
+
   it("should return false when number is not divisible by 4", () => {
-    const leapYearChecker = new LeapYearChecker();
     expect(leapYearChecker.isLeapYear(1997)).toBe(false);
   });
 
   it("should return true when year is divisible by 4", () => {
-    const leapYearChecker = new LeapYearChecker();
     expect(leapYearChecker.isLeapYear(1996)).toBe(true);
   });
 });
